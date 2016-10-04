b08ff03146a41650aa2f70223368ee0be2edc809
peterysc <peterysc@gmail.com>
2016-10-03 23:57:48 -0700

    complete

---
+++ UniqueEmail.java
@@ -0,0 +1,45 @@
+package hackerrank;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+// run time of O(n)
+// space O(n) - doublecheck for hashmap at a large number of keys
+
+public class UniqueEmail {
+	
+	public static void main(String[] args) throws IOException{
+		BufferedReader in = new BufferedReader(new FileReader("src/hackerrank/emailListTest.txt"));
+		String currentLine;
+		HashMap<String,Integer> urlList = new HashMap<String,Integer>();
+		Queue<String> urlOrder = new LinkedList<>();
+		int count;
+		
+		while((currentLine = in.readLine()) != null){
+			if(!urlList.containsKey(currentLine)){
+				count = 1;
+				urlList.put(currentLine, count);
+			}else{
+				count = urlList.get(currentLine);
+				count += 1;
+				urlList.put(currentLine, count);
+			}
+			urlOrder.add(currentLine);
+		}
+		while(urlOrder.peek()!=null){
+			String first = urlOrder.peek();
+			int occurence = urlList.get(first);
+			if(occurence == 1){
+				System.out.println(first);
+			}
+			urlOrder.poll();
+		}
+		in.close();
+	}
+
+}
