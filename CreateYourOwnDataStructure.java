8d4e4a793f8b42cf354b8b6076899510b16d9dc7
peterysc <peterysc@gmail.com>
2016-10-05 14:21:09 -0700

    constraints for data structure

--- CreateYourOwnDataStructure.java
+++ CreateYourOwnDataStructure.java
@@ -1,5 +1,13 @@
 package hackerrank;
 
+/*
+ * create your own data structure that handles the following operation in O(1) time
+ * add, delete, search and findRandom
+ * 
+ * Your data structure will have integers stored in it 
+ * 
+ * find random finds any random integers from your data structure
+ */
 public class CreateYourOwnDataStructure {
 
 }
